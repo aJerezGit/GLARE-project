01c35a1e84a3779a7e12df602c734c6067a4a757
Andres Jerez <andresjerezg@hotmail.com>
2019-07-12 13:13:38 -0500

    AR project

---
+++ Assets/Vuforia/Scripts/MainManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+public class MainManager : MonoBehaviour
+{
+    public GameObject[] videoPlayer;
+    public int CurrentPlayer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DisableAll();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play(int video)
+    {
+        videoPlayer[video].SetActive(true);
+
+        videoPlayer[video].GetComponent<VideoPlayer>().Play();    
+    }
+
+    public void DisableAll() {
+        foreach (var item in videoPlayer)
+        {
+            item.SetActive(false);
+        }
+    }
+}
---
+++ Assets/streamVideo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+public class streamVideo : MonoBehaviour
+{
+    public RawImage rawImage;
+    public VideoPlayer videoPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PlayVideo());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator PlayVideo() {
+        videoPlayer.Prepare();
+        WaitForSeconds waitForSeconds = new WaitForSeconds(1);
+        while (!videoPlayer.isPrepared) {
+            yield return waitForSeconds;
+            break;
+        }
+        rawImage.texture = videoPlayer.texture;
+        videoPlayer.Play();
+    }
+
+}
+
